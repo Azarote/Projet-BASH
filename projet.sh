b395146d6a50f32efe8aa7b581ab0351f92e8937
MrLucas777 <lucaspol@hotmail.fr>
2019-11-21 15:29:16 +0100

    Update projet.sh

--- projet.sh
+++ projet.sh
@@ -4,8 +4,18 @@
 
 sortieFile="sortie.txt"
 resultat="resultat.txt"
+
+#On teste si les fichiers existent
+if [ -f $resultat ]
+then 
 rm $resultat
+fi
+if [ -f $sortieFile ]
+then 
 rm $sortieFile
+fi
+
+
 echo "- Outil de comparaison de fichiers -"
 
 #demande des répertoires
@@ -46,31 +56,53 @@ echo "Début d'analyse... Patientez"
 for i in `find $dir1 -type f`
 	do 
 		md5sum $i >> $sortieFile
-		nbligne1=`expr $nbligne1 + 1`
 	done
 		
 for j in `find $dir2 -type f`
 	do 
 		md5sum $j >> $sortieFile
-		nbligne2=`expr $nbligne2 + 1`
 	done
-	
+
+#On récupère les md5 des 2 répertoires
 temp1=`find $dir1 -type f -exec md5sum {} \; > tempmd5 && md5sum tempmd5 && rm tempmd5 | cut -d ' ' -f1`
 temp2=`find $dir2 -type f -exec md5sum {} \; > tempmd5 && md5sum tempmd5 && rm tempmd5 | cut -d ' ' -f1`
+
 nbfichier1=`find $dir1 -type f | wc -l`
 nbfichier2=`find $dir2 -type f | wc -l`	
-nbtot=$(( $nbfichier1 + $nbfichier2 ))
+nbtot=$(($nbfichier1 + $nbfichier2))
 filedif=`cat $sortieFile | cut -d ' ' -f1 | sort -u | wc -l`
+
+echo "Les MD5 de chaque fichier différent:"
 cat $sortieFile | cut -d ' ' -f1 | sort -u > $resultat
+cat $resultat
 
 #Test pour sortir les fichiers différents
 k=1
+m=1
 while  [ $k -le $filedif ]
 	do
-		sed -n $k'p' resultat.txt && grep `sed -n $k'p' resultat.txt` sortie.txt | cut -d ' ' -f3 > test.txt
-		k=$((k+1))
+		
+		actualmd5=`sed -n $k'p' resultat.txt`
+		echo $k
+		echo "Fichier(s) portant ce md5 : " $actualmd5 >> test.txt
+		k=$(($k+1))
+		m=1
+		
+		while [ $m -le $nbtot ]
+			do
+				md5atest=`sed -n $m'p' sortie.txt | cut -d ' ' -f1` 
+				echo "M - $m - $md5atest"
+				if [ "$actualmd5" = "$md5atest" ]
+					then
+					echo "OUUUI"
+					sed -n $m'p' sortie.txt | cut -d ' ' -f3 >> test.txt
+				else      
+					echo "NOP"
+				fi
+				m=$(($m+1))	
+			done
 	done
-
+	
 #Comparaison des aborescences
 if [ "$temp1" = "$temp2" ]
 	then
@@ -79,7 +111,6 @@ else
 	echo "Les deux arborescences sont différentes."
 fi
 	
-echo "Les résultats sont dans le fichier $resultat"
 echo "Il y a un total de" $nbtot "fichiers dans les arborescences."
 echo "Il y a" $filedif "fichiers différents."
 echo "La liste des fichiers différents est dans le fichier $resultat"

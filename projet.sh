95a698f2e770a209e773dddcbe67391dd26ef7b0
MrLucas777 <lucaspol@hotmail.fr>
2019-11-29 11:05:11 +0100

    Update projet.sh

--- projet.sh
+++ projet.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
-
-# Script projet 2019 M1101 - Systèmes
+#
+#=============================================
+#
+# Script projet BASH 2019 M1101 - Systèmes
 # MUNOZ Matteo - POLLET Lucas - CORIZZI Ianis
 # Groupe X 
+#
+#=============================================
+
 
 sortieFile="sortie.txt"
 resultat="resultat.txt"
-range="fichier-rangé.txt"
-diffe="différence.txt"
-#On teste si les fichiers existent
-if [ -f $resultat ]
-then 
-rm $resultat
-fi
-if [ -f $sortieFile ]
-then 
-rm $sortieFile
-fi
-if [ -f $range ]
-then
-rm $range
-fi
+listedossier1="listedossier1.txt"
+listedossier2="listedossier2.txt"
+listedossiermd51="listedossiermd51.txt"
+listedossiermd52="listedossiermd52.txt"
+tempsousdossier="tempsousdossier.txt"
+tempsousdossier2="tempsousdossier2.txt"
 
+# Couleurs (gras)
+ROUGE="$(tput bold ; tput setaf 1)"
+VERT="$(tput bold ; tput setaf 2)"
+JAUNE="$(tput bold ; tput setaf 3)"
+BLEU="$(tput bold ; tput setaf 4)"
+CYAN="$(tput bold ; tput setaf 6)"
+RESET="$(tput sgr0)"
 
-echo "- Outil de comparaison de fichiers -"
+#Fonction pour supprimer un fichier si il existe
+delFile(){
+	if [ -f $1 ]
+	then 
+		rm $1
+	fi
+}
+
+echo "${ROUGE}- Outil de comparaison de fichiers -${RESET}"
+
+delFile $sortieFile
+delFile $listedossier1
+delFile $listedossier2
+delFile $listedossiermd51
+delFile $listedossiermd52
+delFile $tempsousdossier
+delFile $tempsousdossier2
 
 #demande des répertoires
 
@@ -56,24 +75,120 @@ else
 	dir1=$1
 	dir2=$2
 fi
-	
+
+basedir1=`basename $dir1`
+basedir2=`basename $dir2`
+
+filetri=$basedir1"_"$basedir2"_tri.txt"
+
+delFile $filetri
+
 #début analyse
 echo "Début d'analyse... Patientez"
 
 for i in `find $dir1 -type f`
 	do 
-		md5sum $i >> $sortieFile #récupération de toute les empreintes du premier répertoire 
+		md5sum $i >> $sortieFile #récupération de toute les empreintes du premier répertoire
+		nbligne1=`expr $nbligne1 + 1`
 	done
 		
 for j in `find $dir2 -type f`
 	do 
 		md5sum $j >> $sortieFile #récupération de toute les empreintes du deuxième répertoire
+		nbligne2=`expr $nbligne2 + 1`
 	done
-
-#On récupère les md5 des 2 répertoires
+		
 temp1=`find $dir1 -type f -exec md5sum {} \; > tempmd5 && md5sum tempmd5 && rm tempmd5 | cut -d ' ' -f1`
 temp2=`find $dir2 -type f -exec md5sum {} \; > tempmd5 && md5sum tempmd5 && rm tempmd5 | cut -d ' ' -f1`
 
+#############################################################
+# PARTIE A CONTINUER ....
+
+find $dir1 -type d >> listedossier1.txt
+find $dir2 -type d >> listedossier2.txt
+
+#arbo1
+while read line
+	do
+		find $line -type f -exec md5sum {} \; >> tempsousdossier.txt
+		find $line -type d -exec basename {} \; | sed '1d' >> tempsousdossier2.txt
+		while read line2
+			do
+				chemin=`echo "$line2" | cut -d ' ' -f3` 
+				nom=`basename $chemin`
+				md5=`echo "$line2" | cut -d ' ' -f1`
+				echo "$md5" "$nom" >> tempsousdossier2.txt
+			done < tempsousdossier.txt
+		chemin=`echo "$line" | cut -d ' ' -f3` 
+		md5=`md5sum tempsousdossier2.txt | cut -d ' ' -f1`
+		echo "$md5" "$chemin" >> listedossiermd51.txt
+		
+		rm -f tempsousdossier.txt
+		rm -f tempsousdossier2.txt
+	done < listedossier1.txt
+
+#arbo2
+while read line
+	do
+		find $line -type f -exec md5sum {} \; >> tempsousdossier.txt
+		find $line -type d -exec basename {} \; | sed '1d' >> tempsousdossier2.txt
+		while read line2
+			do
+				chemin=`echo "$line2" | cut -d ' ' -f3` 
+				nom=`basename $chemin`
+				md5=`echo "$line2" | cut -d ' ' -f1`
+				echo "$md5" "$nom" >> tempsousdossier2.txt
+			done < tempsousdossier.txt
+		chemin=`echo "$line" | cut -d ' ' -f3` 
+		md5=`md5sum tempsousdossier2.txt | cut -d ' ' -f1`
+		echo "$md5" "$chemin" >> listedossiermd52.txt
+		
+		rm -f tempsousdossier.txt
+		rm -f tempsousdossier2.txt
+	done < listedossier2.txt
+
+#Comparaison arbo1 par rapport à arbo2
+while read line
+	do
+		bool=0
+		linemd5=`echo "$line" | cut -d ' ' -f1`
+		linechemin=`echo "$line" | cut -d ' ' -f2`
+		while read line2
+			do
+				line2md5=`echo "$line2" | cut -d ' ' -f1`
+				if test "$linemd5" == "$line2md5"
+				then
+					bool=1
+				fi
+			done < listedossiermd52.txt
+		if test $bool -eq 0 
+		then
+			echo "Le dossier à l'adresse : $linechemin n'apparait dans l'arborescence 2"
+		fi
+	done < listedossiermd51.txt
+
+#Comparaison arbo2 par rapport à arbo1	
+while read line
+	do
+		bool=0
+		linemd5=`echo "$line" | cut -d ' ' -f1`
+		linechemin=`echo "$line" | cut -d ' ' -f2`
+		while read line2
+			do
+				line2md5=`echo "$line2" | cut -d ' ' -f1`
+				if test "$linemd5" == "$line2md5"
+				then
+					bool=1
+				fi
+			done < listedossiermd51.txt
+		if test $bool -eq 0 
+		then
+			echo "Le dossier à l'adresse : $linechemin n'apparait dans l'arborescence 1"
+		fi
+	done < listedossiermd52.txt
+
+#####################################################	
+	
 nbfichier1=`find $dir1 -type f | wc -l`
 nbfichier2=`find $dir2 -type f | wc -l`	
 nbtot=$(($nbfichier1 + $nbfichier2))
@@ -82,22 +197,41 @@ filedif=`cat $sortieFile | cut -d ' ' -f1 | sort -u | wc -l`
 echo "Les MD5 de chaque fichier différent:"
 cat $sortieFile | cut -d ' ' -f1 | sort -u > $resultat #récupère toute les empreintes différentes
 cat $resultat
-sort $sortieFile >> $range #range les fichier par empreintes
-#récupération des différents fichier et répertoire pour chaque arborescence
-diff $dir1 $dir2 >> $diffe
 
-#Comparaison des aborescences
-if [ "$temp1" = "$temp2" ]
-	then
-	echo "Les deux arborescences sont identiques."
-else
-	echo "Les deux arborescences sont différentes."
-fi
+#Test pour sortir les fichiers différents
+k=1
+m=1
+while  [ $k -le $filedif ]
+	do
+		actualmd5=`sed -n $k'p' $resultat`
+		echo "Fichier(s) portant ce md5 : $actualmd5" >> $filetri
+		k=$(($k+1))
+		m=1
+		
+		while [ $m -le $nbtot ]
+			do
+				md5atest=`sed -n $m'p' $sortieFile | cut -d ' ' -f1` 
+				
+				if [ "$actualmd5" = "$md5atest" ]
+					then
+					sed -n $m'p' $sortieFile | cut -d ' ' -f3 >> $filetri
+				fi
+				m=$(($m+1))	
+			done
+		echo "" >> $filetri
+	done
 	
-echo "Il y a un total de" $nbtot "fichiers dans les arborescences."
-echo "Il y a" $filedif "fichiers différents."
-echo "La liste des fichiers avec des empreintes différentes est dans le fichier $resultat"
-echo "La liste de tout les fichier rangé par empreinte md5 est dans $range"
-#com a supprimer avans de rendre le projet (étas de ce que vous avais fait est pourquois)
-#le prof a dit que sa servais a rien de sortir les fichier différence donc je ai suprimé cette partie la 
-#reste les fichier et répertoire différent pour chaque arborescences (machin avec diff) 
+#Comparaison des aborescences
+if [ "$temp1" == "$temp2" ]
+	then
+	echo "Les deux arborescences sont ${JAUNE}identiques${RESET}."
+	nbtot=$nbfichier1
+else
+	echo "Les deux arborescences sont ${JAUNE}différentes${RESET}."
+fi
+
+echo "Il y a un total de ${VERT}"$nbtot" fichiers ${RESET}dans les arborescences."
+echo "Il y a ${VERT}"$filedif" fichiers ${RESET}qui ont un md5 différent."
+echo "L'arborescence" $dir1 "possède x fichiers différents de" $dir2
+
+delFile $resultat
